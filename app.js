4558bf7c6bcc4abbe121f5fdb8fbe31242e660a7
alpha951 <20uec068@lnmiit.ac.in>
2023-06-28 12:00:52 +0530

    public dir path

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.engine('ejs', require('ejs').renderFile);
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const dbConnect = require("./db/dbconnect");
 const postRouter = require("./routes/post");
